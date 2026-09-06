0350c4da2239a4b4a6a6dd1e80ca42802e7783f8
ruftyboy <67052103+ruftyboy@users.noreply.github.com>
2026-09-06 14:45:40 +0300

    Import natural angular geometry bridge

--- RACEFormal.lean
+++ RACEFormal.lean
@@ -10,6 +10,7 @@ import RACEFormal.SoftRaceBit
 import RACEFormal.CornerFactorization
 import RACEFormal.ActualRaceSampler
 import RACEFormal.GaussianHyperplaneSampler
+import RACEFormal.AngularGeometryBridge
 import RACEFormal.ActualRaceHypotheses
 import RACEFormal.SoftmaxProductEquivalence
 import RACEFormal.GaussianLeakage
